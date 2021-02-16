895640b8b16f6ee9d7e6becc33852dff19463d58
Nick Belbas <nickbelbas@earthlink.net>
2021-02-15 18:58:48 -0600

    Added comments and documentation
    - Added comments to the code
    - Added info into README regarding original source of project and inspiration
    - Outlined improvements and modifications in README
    - Removed glucose for stability

--- FunProgram.py
+++ FunProgram.py
@@ -5,7 +5,11 @@ import string
 import json
 import time
 
-def makeFakeCCNum():
+#################################################
+#    Generator Functions
+#################################################
+
+def makeFakeCCNum(): #Function that generates a fake credit card number in the form "#### #### #### #### ####"
     firstSet=str(random.randint(1000,9999))
     secondSet=str(random.randint(1000,9999))
     thirdSet=str(random.randint(1000,9999))
@@ -13,74 +17,76 @@ def makeFakeCCNum():
     fifthSet=str(random.randint(1000,9999))
     return "{} {} {} {} {}".format(firstSet,secondSet,thirdSet,fourthSet,fifthSet)
 
-def makeFakeCCexp():
+def makeFakeCCexp(): #Function that generates a fake credit card exp date in the form "MM/YY"
     month=str(random.randint(10,12))
     year=str(random.randint(21,28))
     return "{}/{}".format(month,year)
 
-def makeFakeCCcvv():
+def makeFakeCCcvv(): #Function that returns a three digit random number to be used as the CVV of the fake credit card
     return str(random.randint(100,999))
 
-def makeFakePinNum():
+def makeFakePinNum(): #Function that returns a 4 digit random number to be used as the PIN number for the credit card (some sites ask for this)
     return str(random.randint(1000,9999))
 
-def makeFakeDOB():
+def makeFakeDOB(): #Function that creates a random date of birth in the form "MM/DD/YYYY"
     month=str(random.randint(1,12))
     day=str(random.randint(1,28))
     year=str(random.randint(1970,2002))
     return "{}/{}/{}".format(month,day,year)
 
-def makeFakeSSN():
+def makeFakeSSN(): #Function that creates a fake Social Security Number in the form "###-##-####"
     firstOne=str(random.randint(100,999))
     secondOne=str(random.randint(10,99))
     thirdOne=str(random.randint(1000,9999))
     return "{}-{}-{}".format(firstOne,secondOne,thirdOne)
 
 
-requests.adapters.DEFAULT_RETRIES=10
+chars = string.ascii_letters + string.digits + '!@#$%^&*()' #Create a list of all possible characters for password generator to use
+random.seed = (os.urandom(1024)) #Seed the random number generator
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-random.seed = (os.urandom(1024))
+url='scam url here' #This is where you will copy/paste the link you found from the video demonstration
+headers = {'User-Agent': 'Mozilla/5.0 (Win64 x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'} #Browser user agent header, eventually I'll make a function that selects from a list of possible headers to further obfuscate
 
-url='scam url here'
-headers = {'User-Agent': 'Mozilla/5.0 (Win64 x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
+names = json.loads(open('names.json').read()) #Open the names.json file containing first names and create a list containing the entries
+lastNames=json.loads(open('surnames.json').read()) #Open the surnames.json file containing last names and create a list containing the entries
 
-names = json.loads(open('names.json').read())
-lastNames=json.loads(open('surnames.json').read())
+#Email Addresses - Currently a list of email domains, but I eventually will expand the list and put it into a json file like the names files
 emailAddresses=["gmail.com","yahoo.com","sjcd.edu","outlook.com", "youtube.com", "nasa.gov", "enron.com", "vxinnovations.com", "fbi.gov", "doj.gov", "cia.gov", "nsa.gov", "protonmail.com", "protonmail.ch","thecw.com","aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com","google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com","live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk","btinternet.com", "virginmedia.com", "blueyonder.co.uk", "freeserve.co.uk", "live.co.uk","ntlworld.com", "o2.co.uk", "orange.net", "sky.com", "talktalk.co.uk", "tiscali.co.uk","virgin.net", "wanadoo.co.uk", "bt.com","yahoo.ca", "hotmail.ca", "bell.net", "shaw.ca", "sympatico.ca", "rogers.com"]
 
+while (1==1): #Run this forever
+    try:  #Try/Catch to allow for error recovery like page timeouts
 
-while (1==1):
-    try:
-        for i in range(len(lastNames)):
-            randomName=random.randint(0,len(names)-1) #Random First Name
-            randomLastName=random.randint(0,len(lastNames)-1) #Random Last Name
+        randomName=random.randint(0,len(names)-1) #Select a random first name from the list
+        randomLastName=random.randint(0,len(lastNames)-1) #Select a random last name from the list
 
-            extraLength=random.randint(1,5)
-            name_extra = ''.join(random.choice(string.digits) for i in range(extraLength))
-            chosenAddress=str(random.choice(emailAddresses))
+        extraLength=random.randint(1,5)
+        name_extra = ''.join(random.choice(string.digits) for i in range(extraLength)) #Create a string of 1-5 random characters to add onto front of name, useful for creating usernames and email addresses
 
-            username = names[randomName].lower() + name_extra + "@" + chosenAddress #Randomly generated email
-            passwordLength=random.randint(8,15)
-            password = ''.join(random.choice(chars) for i in range(passwordLength)) #Randomly generated password
+        chosenAddress=str(random.choice(emailAddresses)) #Select a random email domain from the list
 
-            CreditCardNumber=makeFakeCCNum()
-            CreditCardExpDate=makeFakeCCexp()
-            CreditCardCVV=makeFakeCCcvv()
-            CreditCardPin=makeFakePinNum()
-            FakeDOB=makeFakeDOB()
-            FakeSSN=makeFakeSSN()
+        #Randomly generated email which consists of "(randomly selected first name in lowercase) (1-5 random characters) @ (randomly selected email domain)"
+        username = names[randomName].lower() + name_extra + "@" + chosenAddress
 
-            requests.post(url, allow_redirects=False, data={
-                'cc': CreditCardNumber,
-                'exp': CreditCardExpDate,
-                'cvv':CreditCardCVV,
-                'pin':CreditCardPin,
-                'dob':FakeDOB,
-                'mname': randomName,
-                'sc': FakeSSN,
-            },headers=headers)
-            print 'sending packet:%s, %s, %s,%s,%s,%s,%s' % (CreditCardNumber,CreditCardExpDate, CreditCardCVV, CreditCardPin, FakeDOB, randomName, FakeSSN) #edit to only show data points being sent
+        passwordLength=random.randint(8,15)
+        password = ''.join(random.choice(chars) for i in range(passwordLength)) #Create a randomly generated 8-15 digit password using the list of characters from earlier
+
+        CreditCardNumber=makeFakeCCNum()   #Examples of calling each of the generator functions from the beginning of the code
+        CreditCardExpDate=makeFakeCCexp()
+        CreditCardCVV=makeFakeCCcvv()
+        CreditCardPin=makeFakePinNum()
+        FakeDOB=makeFakeDOB()
+        FakeSSN=makeFakeSSN()
+
+        requests.post(url, allow_redirects=False, data={  #Use the requests library to send the data in json format using an HTTP POST command, the names of each field will vary and you will need to use the video tutorial to find the ones for your case
+            'cc': CreditCardNumber,
+            'exp': CreditCardExpDate,
+            'cvv':CreditCardCVV,
+            'pin':CreditCardPin,
+            'dob':FakeDOB,
+            'mname': randomName,
+            'sc': FakeSSN,
+        },headers=headers)
+        print 'sending packet:%s, %s, %s,%s,%s,%s,%s' % (CreditCardNumber,CreditCardExpDate, CreditCardCVV, CreditCardPin, FakeDOB, randomName, FakeSSN) #Mainly for debugging, just a statement to show what data points are being sent at the moment
     except:
         print "Error, restarting...\n" #If page times out (common through VPN), wait 5 seconds and restart
-    time.sleep(5)
+        time.sleep(5)
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Phishermens Nightmare
- Flood a scammer's phishing page with fake user data
+# Phishermen's Nightmare
+
+I saw a YouTube video where a programmer received a phishing attempt in his email and proceeded to create a Python program to flood the scammer's database with fake personal data. I liked the idea and copied it but I noticed he never updated the code and there were some improvements that could be made. I decided to make those improvements are occasionally add new functions whenever I see the need.
+
+
+## Original Project
+
+The original project video is on the **Engineer Man** YouTube Channel and is a good demonstration of the process of writing the code and finding the exact URL that the post request needs to use. I recommend watching this video and follow his method for finding the URL. My code was heavily based off of his code linked in the description of the video.
+Original video: https://www.youtube.com/watch?v=UtNYzv8gLbs
+
+## Modifications
+
+The original project was good but it had a few flaws. All the emails were a first name and a single digit followed by "@yahoo.com". Additionally, all the passwords were 8 characters long. A smart person could probably run a command in their database to find all entries from yahoo with 8-character passwords. I modified the code to be a bit smarter.
+* Passwords are now anywhere between 8 and 15 characters in length, randomly generated for each entry
+* Email addresses now have between 1 and 5 numbers trailing the name and before the "@"
+* The code now selects from a decent list of possible email domains and selects a new one for each generated entry
+## Additions
+
+Some phishing sites ask for more then just a username and password, some ask for credit card details, maiden names, or even social security numbers! I created a few simple functions to generate some of these common bits of info and will add more as I see the demand. Each of the functions needs to parameters when it is called and it simply returns a string with the data. The functions I have so far create:
+* Credit Card Numbers (currently just random numbers but I hope to incorporate the algorithm such that cards numbers are the correct pattern)
+* Credit Card CVV and PIN numbers (literally just returning either 3 or 4 random digits)
+* Credit Card Expiration Dates (just returns the a string with a month/year in 2-digit number form)
+* Date of Birth (returns a string with the 2-digit day and month and a 4-digit year in MM/DD/YYYY format)
+* More to come soon!
+
+## Final Remarks
+This code is written in Python 2.7.16 and you need to have the "requests" module installed in order for this code to work
