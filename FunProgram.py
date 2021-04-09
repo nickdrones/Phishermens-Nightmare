1c4d359ad9ada714a1f5c669772de533ef08ee4e
Nick Belbas <nickbelbas@earthlink.net>
2021-04-08 22:53:06 -0500

    Added ability to create fake address
    - Added a function for making a fake street address, using json list of streets in Houston. WIll add more cities from around the US later.
    - Added a function to generate an "ID" number with the request since some places now incorporate that to prevent credential stuffing
    - Currently only has Houston, TX, USA as the city/state/country combo, but I will create new json files with more cities, states, and countries eventually.

--- FunProgram.py
+++ FunProgram.py
@@ -46,18 +46,33 @@ def makeFakePhoneNum(): #Function that creates a fake phone number in the form "
     thirdOne=str(random.randint(1000,9999))
     return "(+1){}-{}-{}".format(firstOne,secondOne,thirdOne)
 
+def makeFakeAddress():
+    houseNum=str(random.randint(10000,99999))
+    randomStreet=random.randint(0,len(streetNames)-1)
+    streetText=str(streetNames[randomStreet].lower())
+    return "{} {}".format(houseNum,streetText)
+
+def makeZipCode():
+    zipCode=str(random.randint(10000,99999))
+    return "{}".format(zipCode)
+
+def makeIDnumber():
+    IDnumber=str(random.randint(100000000000000000,999999999999999999))
+    return "{}".format(IDnumber)
+
 
 chars = string.ascii_letters + string.digits + '!@#$%^&*()' #Create a list of all possible characters for password generator to use
 random.seed = (os.urandom(1024)) #Seed the random number generator
 
-url='scam url here' #This is where you will copy/paste the link you found from the video demonstration
+url='url' #This is where you will copy/paste the link you found from the video demonstration
 
 #Browser user agent header, eventually I'll make a function that selects from a list of possible headers to further obfuscate
-headers = {'User-Agent': 'Mozilla/5.0 (Win64 x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'} 
+#headers = {'User-Agent': 'Mozilla/5.0 (Win64 x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'} 
 
 names = json.loads(open('names.json').read()) #Open the names.json file containing first names and create a list containing the entries
 lastNames=json.loads(open('surnames.json').read()) #Open the surnames.json file containing last names and create a list containing the entries
 emailAddresses=json.loads(open('emailDomains.json').read()) #Open the emailDomains.json file containing email domains and create a list containing the entries
+streetNames=json.loads(open('streetNames.json').read())
 
 while (1==1): #Run this forever
     try:  #Try/Catch to allow for error recovery like page timeouts
@@ -82,16 +97,38 @@ while (1==1): #Run this forever
         FakeDOB=makeFakeDOB()
         FakeSSN=makeFakeSSN()
 
-        requests.post(url, allow_redirects=False, data={  #Use the requests library to send the data in json format using an HTTP POST command, the names of each field will vary and you will need to use the video tutorial to find the ones for your case
-            'cc': CreditCardNumber,
-            'exp': CreditCardExpDate,
-            'cvv':CreditCardCVV,
-            'pin':CreditCardPin,
-            'dob':FakeDOB,
-            'mname': randomName,
-            'sc': FakeSSN,
-        },headers=headers)
-        print 'sending packet:%s, %s, %s,%s,%s,%s,%s' % (CreditCardNumber,CreditCardExpDate, CreditCardCVV, CreditCardPin, FakeDOB, randomName, FakeSSN) #Mainly for debugging, just a statement to show what data points are being sent at the moment
+        fakeFirstName=names[randomName].lower()
+        fakeLastName=lastNames[randomLastName].lower()
+
+        fakeIDnumber=makeIDnumber()
+
+        fakePhoneNumber=makeFakePhoneNum()
+        countryTheyreFrom="USA"
+        fakeAddress = makeFakeAddress()
+        zipcodeOfHouse = makeZipCode()
+        cityTheyFrom="Houston"
+        stateTheyFrom = "TX"
+
+        response=requests.post(url, allow_redirects=False, data={  #Use the requests library to send the data in json format using an HTTP POST command, the names of each field will vary and you will need to use the video tutorial to find the ones for your case
+            '_u976696156260240105[first]': fakeFirstName,
+            '_u976696156260240105[last]': fakeLastName,
+            '_u692438108750404134[line1]': fakeAddress,
+            '_u692438108750404134[line2]': "",
+            '_u692438108750404134[city]': cityTheyFrom,
+            '_u692438108750404134[state]': stateTheyFrom,
+            '_u692438108750404134[zip]': zipcodeOfHouse,
+            '_u692438108750404134[country]': countryTheyreFrom,
+            '_u924478738247396162': username,
+            '_u423012236818616891[number]': fakePhoneNumber,
+            'wsite_subject':  "",
+            'form_version': 2,
+            'wsite_approved': "approved",
+            'ucfid': fakeIDnumber,
+            'recaptcha_token': ""
+        })
+        print ""
+        print "%s" % response
+        print 'sending packet:%s, %s, %s, %s, %s, %s, %s, %s, %s, ID number: %s' % (fakeFirstName,fakeLastName, fakeAddress, cityTheyFrom, stateTheyFrom, str(zipcodeOfHouse), countryTheyreFrom, username,str(fakePhoneNumber), str(fakeIDnumber)) #Mainly for debugging, just a statement to show what data points are being sent at the moment
     except:
         print "Error, restarting...\n" #If page times out (common through VPN), wait 5 seconds and restart
         time.sleep(5)
